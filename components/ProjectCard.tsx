4af8b544062d63731575f5e058ff9493dd8dc875
msclal <mika1shanela@gmail.com>
2023-02-28 20:35:52 -0800

    fixed movile

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -10,13 +10,13 @@ export const ProjectCard = ({ title, description }: Props) => {
   return (
     <Link
       href={`${title.toLocaleLowerCase()}`}
-      className="flex flex-col border-2 items-center border-amber-900 py-4 md:py-10 w-64 md:w-1/4 my-3 rounded-xl cursor-pointer hover:bg-green-100"
+      className="flex flex-col border-2 items-center border-amber-900 md:py-10 w-64 md:w-1/4 my-3 rounded-xl cursor-pointer hover:bg-green-100 p-4"
     >
       {/* <div className="flex flex-col border-2 items-center border-amber-900 py-16 w-full md:w-1/4 my-3 rounded-xl cursor-pointer hover:bg-green-100"> */}
       <p className="text-amber-900 font-semibold text-3xl text-center">
         {title}
       </p>
-      <p className="text-amber-900 text-xl p-5 text-center">{description}</p>
+      <p className="text-amber-900 text-xl text-center">{description}</p>
       {/* </div> */}
     </Link>
   );
